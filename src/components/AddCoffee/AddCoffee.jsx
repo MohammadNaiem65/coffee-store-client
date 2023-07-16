3a6caf4b78d939685d5426f67e85b5141d4c1bf6
MohammadNaiem65 <mohammadnaiem661065@gmail.comm>
2023-07-16 17:15:30 +0600

    made the initial setup

---
+++ src/components/AddCoffee/AddCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddCoffee = () => {
+    return (
+        <div>
+            This is the add coffee page.
+        </div>
+    );
+};
+
+export default AddCoffee;
---
+++ src/components/UpdateCoffee/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            This is the update coffee page.
+        </div>
+    );
+};
+
+export default UpdateCoffee;
